6e08a1925b4d6d9cbfbdd6dc9a8abad0a0dd8556
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-25 18:42:31 +0000

    Printer class and pages test passed

---
+++ src/main/java/Printer.java
@@ -0,0 +1,12 @@
+public class Printer {
+
+    private int pages;
+
+    public Printer(int pages){
+        this.pages = pages;
+    }
+
+    public int getPages(){
+        return this.pages;
+    }
+}
