ca21c980de6075ec7b76f8aa720b2601c9e9bb64
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-25 17:39:20 +0000

    multiply test passed

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,12 +7,17 @@ public class CalculatorTest {
   Calculator calculator = new Calculator();
 
   @Test
-    public void canAdd(){
+  public void canAdd(){
       assertEquals(8, calculator.add(4,4));
   }
 
   @Test
-    public void canSubtract(){
+  public void canSubtract(){
       assertEquals(10, calculator.subtract(15, 5));
+  }
+
+  @Test
+  public void canMultiply(){
+    assertEquals(4, calculator.multiply(2, 2));
   }
 }
